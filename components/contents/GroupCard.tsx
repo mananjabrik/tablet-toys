b6b7bdb5fc7461033a83d1759e120048949b17fc
mananjabrik <manan.jabrik@gmail.com>
2021-10-06 18:08:43 +0700

    update if data error

--- components/contents/GroupCard.tsx
+++ components/contents/GroupCard.tsx
@@ -6,16 +6,16 @@ export const GroupCard: React.FC<GroupCardProps> = (props) => {
 	return (
 		<Stack bg="#484848" p="1rem" w="full" rounded="md">
 			<Heading size="md" fontWeight="medium">
-				{props.titleGroup}
+				{props?.titleGroup}
 			</Heading>
 			<Stack px="10px">
 				<Box>
 					<Text>Whos in this group</Text>
-					<Text>
-						{props.users?.map((user) => {
-							return `${user}, `;
+					<Stack direction="row">
+						{props.users?.map((user, idx) => {
+							return <Text key={idx}>{user}</Text>;
 						})}
-					</Text>
+					</Stack>
 				</Box>
 				<Box>
 					<Text>Scene Squence</Text>
